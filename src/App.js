b08861770182daff8ef0da82669ef1b9bc9ea8db
earno12 <earnold2010@gmail.com>
2023-01-07 18:23:09 -0600

    Added new title to loading page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2>Welcome to</h2>
+        <h1>The Goat Lounge.</h1>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
