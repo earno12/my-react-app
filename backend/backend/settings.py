bb9cf801e434afc0ea4b4e1594fe86659ecee18f
earno12 <earnold2010@gmail.com>
2023-01-12 21:46:30 -0600

    removed django secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%#*1oef_sqh5xwa&9*eg0k9mtup_isl*-+#p9ar!9&m1*%h&cw'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
